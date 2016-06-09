5a7e420dfe3f6dcd6a4509a70440f1863c8d1f26
Subhankar Ghosh <microlifecc@gmail.com>
2016-06-09 18:18:36 +0530

    Some edits

--- hidden.py
+++ hidden.py
@@ -2,7 +2,7 @@
 # https://apps.twitter.com/
 
 def oauth() :
-    return { "consumer_key" : "AqbJgqXd9ZTF3bzzEPnj4htPR",
-        "consumer_secret" : "7dOJwxCHxRx4T2DQ1BBjY0M9wZmLNgfDJbmspmYlTWYUpyotnk",
-        "token_key" : " 2355246230-jcZnogxyesZgkSsb8MSH3Nmx7YWvBMyEZXvOisP",
-        "token_secret" : "4h3PqcYQPdVFjW2sGiqn8f6ijR24FgmGNw2gREDdSaM3G" }
+    return { "consumer_key" : "AqbJgqXd9***3bzzEPnj4htPR",
+        "consumer_secret" : "7dOJwxCHxRx4T2DQ1****0M9wZmLNgfDJbmspmYlTWYUpyotnk",
+        "token_key" : " 2355246230-jcZnogxyesZgkSsb8***3Nmx7YWvBMyEZXvOisP",
+        "token_secret" : "4h3PqcYQPdVFjW2*****8f6ijR24FgmGNw2gREDdSaM3G" }
